4c623ffa9c1e4700de6c52dd696fb4a11be8d4ac
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-03-07 19:02:29 +0200

    Day4 + Day5

--- Day4/index.php
+++ Day4/index.php
@@ -2,7 +2,7 @@
 
 //open session and loading the composer
 session_start();
-require_once("vendor/autoload.php");
+require_once "vendor/autoload.php";
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
@@ -12,17 +12,17 @@ $capsule->addConnection([
     "host" => _host_,
     "database" => _database_,
     "username" => _username_,
-    "password" => _password_
+    "password" => _password_,
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 $index = (isset($_GET["index"]) && is_numeric($_GET["index"]) && $_GET["index"] > 0) ? (int) $_GET["index"] : 0;
-$all_requrds= Capsule::table("items")->skip($index)->take(_Pager_size_)->get();
-$next_index = $index +_Pager_size_;
+$all_requrds = Capsule::table("items")->skip($index)->take(_Pager_size_)->get();
+$counter = Capsule::table("items")->skip($index)->take(_Pager_size_)->count();
+$next_index = (($index + _Pager_size_) < $counter) ? $index + _Pager_size_ : abs($counter - _Pager_size_);
 $next_link = "http://localhost/iti-labs/Day4/index.php?index=$next_index";
-$previous_index = (($index - _Pager_size_)>=0)?$index - _Pager_size_:0;
+$previous_index = (($index - _Pager_size_) >= 0) ? $index - _Pager_size_ : 0;
 $previous_link = "http://localhost/iti-labs/Day4/index.php?index=$previous_index";
-
 
 //$glasses = Capsule::table('items')->first();
 //$searched_glasses = Capsule::table('items')->find(91);
@@ -30,7 +30,18 @@ $previous_link = "http://localhost/iti-labs/Day4/index.php?index=$previous_index
 //$usa_glasses = Capsule::table("items")->where("Country","=","USA")->get();
 
 //return response view
-  require_once("views/table.php");
+if (isset($_GET["glass"]) && is_numeric($_GET["glass"]) && $_GET["glass"] >= 0) {
+    require_once "views/details.php";
+} else {
+    require_once "views/table.php";
+}
+if (isset($_POST['item'])) {
+    $result = Capsule::table("items")->where("product_name", $_POST['item'])->first();
+    if (Capsule::table('items')->where('product_name',$_POST['item'])->exists()) {
+        require_once "views/result.php";
+    } else {
+        require_once "views/table.php";
+    }
+  }
+
 //require_once("views/intro.php");
-
-
